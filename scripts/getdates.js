7afeed8307c055e1eeda6319cf03b87923377284
Jeremiah Franco <jeremiahfranco3@gmail.com>
2024-01-18 10:31:20 -0700

    Fixed JS and added og:title

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,13 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Get the current year
     var currentYear = new Date().getFullYear();
-    // Display the current year in the copyrightYear span
     document.getElementById("copyrightYear").textContent = currentYear;
-
-    // Get the last modified date of the document
     var lastModifiedDate = new Date(document.lastModified);
-
-    // Format the last modified date to display date and time
     var options = { 
         year: "numeric", 
         month: "long", 
@@ -17,8 +11,6 @@ document.addEventListener("DOMContentLoaded", function() {
         second: "numeric" 
     };
     var formattedLastModifiedDate = lastModifiedDate.toLocaleString(undefined, options);
-
-    // Display the formatted last modified date in the lastModified span
     var lastModifiedSpan = document.getElementById("lastModified");
     lastModifiedSpan.textContent = "Last Modification: " + formattedLastModifiedDate;
 });
