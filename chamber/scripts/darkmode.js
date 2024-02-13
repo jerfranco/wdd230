bd16471e138ae6c5e6235358c315c67d1fd3d16b
Jeremiah Franco <jeremiahfranco3@gmail.com>
2024-02-12 19:33:03 -0700

    Finalized code for updated Chamber website

---
+++ chamber/scripts/darkmode.js
@@ -0,0 +1,43 @@
+// Function to toggle dark mode
+function toggleDarkMode() {
+    var mainElement = document.querySelector('main');
+    var darkModeToggle = document.querySelector('.switch input[type="checkbox"]');
+    var card = document.querySelectorAll('.card p');
+    var weather = document.querySelectorAll('.weathercard p');
+    var weatherIcon = document.querySelectorAll('.weathercard img')
+    var action = document.querySelector('.actionCard p');
+
+    mainElement.classList.toggle('dark-mode');
+
+    if (darkModeToggle.checked) {
+        mainElement.style.backgroundColor = '#fdf0d5';
+         card.forEach(function(paragraph) {
+            paragraph.style.backgroundColor = '';
+            paragraph.style.color = '';
+        });
+        weather.forEach(function(paragraph) {
+            paragraph.style.backgroundColor = '';
+            paragraph.style.color = '';
+        });
+        weatherIcon.forEach(function(paragraph) {
+            paragraph.style.backgroundColor = '';
+        });
+        action.style.color = '';
+        action.style.backgroundColor = '';
+    } else {
+        mainElement.style.backgroundColor = 'black';
+        card.forEach(function(paragraph) {
+            paragraph.style.backgroundColor = '#424242';
+            paragraph.style.color = 'white';
+        });
+        weather.forEach(function(paragraph) {
+            paragraph.style.backgroundColor = '#424242';
+            paragraph.style.color = 'white';
+        });
+        weatherIcon.forEach(function(paragraph) {
+            paragraph.style.backgroundColor = '#424242';
+        });
+        action.style.color = 'white';
+        action.style.backgroundColor = '#424242';
+    }
+}
